64c5e8703460993693c979a0ae23197b1ba6c8d6
Colin Adler <colin@coder.com>
2019-10-01 10:26:38 -0500

    Add flags

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/tatsuworks/dirprefix
 
 go 1.13
 
-require github.com/apple/foundationdb/bindings/go v0.0.0-20190930202504-1f6af42a484e
+require (
+	github.com/apple/foundationdb/bindings/go v0.0.0-20190930202504-1f6af42a484e
+	go.coder.com/flog v0.0.0-20190906214207-47dd47ea0512
+)
--- main.go
+++ main.go
@@ -2,20 +2,37 @@ package main
 
 import (
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"os"
 
 	"github.com/apple/foundationdb/bindings/go/src/fdb"
 	"github.com/apple/foundationdb/bindings/go/src/fdb/directory"
+	"go.coder.com/flog"
 )
 
+var (
+	apiV   int
+	create bool
+)
+
+func init() {
+	flag.IntVar(&apiV, "apiVersion", 610, "FDB API version to use")
+	flag.BoolVar(&create, "create", false, "Create directory if it doesn't exist")
+}
+
 func main() {
-	fdb.MustAPIVersion(610)
+	fdb.MustAPIVersion(apiV)
 	db := fdb.MustOpenDefault()
 
-	dir, err := directory.CreateOrOpen(db, []string{os.Args[1]}, nil)
+	if len(os.Args) != 2 {
+		flog.Error("Please specify one directory")
+		os.Exit(1)
+	}
+
+	dir, err := directory.Open(db, []string{os.Args[1]}, nil)
 	if err != nil {
-		panic(err)
+		flog.Fatal("Failed to open directory: " + err.Error())
 	}
 
 	fmt.Println(hex.EncodeToString(dir.Bytes()))
